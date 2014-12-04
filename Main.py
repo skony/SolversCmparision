2c1dc192c69e9052d9b7cf4382c703f60a6cc682
skony <piotr7skonieczny@gmail.com>
2014-12-05 00:58:02 +0100

    lp_solve version part1

--- Main.py
+++ Main.py
@@ -10,8 +10,11 @@ import input_translator
 from sys import stdout
 import output_scanner
 
+source = ""
+script_dir = ""
 
 def getConfiguredSolvers():
+    global script_dir
     solvers = []
     script_dir = os.path.dirname(__file__)
     os.chdir("solvers")
@@ -27,8 +30,8 @@ def getConfiguredSolvers():
     return solvers
     
 def runSolver(solver, file_name):
-    results = open("RESULTS", 'a')
-    if(solver["input_translator"] == "true"):
+    results = open(script_dir + "/results/" + file_name + "RESULTS", 'a')
+    if(solver["input"] != "lp_solve"):
         trans_cmd = "input_translator." + solver["id"] + "(\"" + file_name + "\")"
         trans_file = eval(trans_cmd)
         command = ""
@@ -47,9 +50,9 @@ def runSolver(solver, file_name):
         command = ""
         for x in solver["run"]:
             if(x == "file_name"):
-                x = file_name
+                x = script_dir + "/problems/" + file_name
             elif(x == "file_out"):
-                x = solver["id"] + "." + solver["out"]
+                x = script_dir + "/outputs/" + file_name + solver["id"] + "." + solver["out"]
             command += x + " "
         t1 = time.time()
         Popen(command, shell=True).wait()
@@ -59,18 +62,22 @@ def runSolver(solver, file_name):
         
 def getProblemParams(file_path):
     problem = open(file_path, 'r')
-    results = open("RESULTS", 'a')
-    pattern = re.compile(".+[:]")
-    NoV = 1
+    os.chdir("../")
+    os.chdir("results")
+    results = open(file_path + "RESULTS", 'a')
+    os.chdir("../")
+    os.chdir("problems")
+    pattern = re.compile("\w+:")
+    NoV = 0
     NoC = 0
     NoViC = 0
     cons = False
     max = 1
     min = 1
-    factor = re.compile("[ ]+[0-9]+[.]{0,1}[0-9]*")
-    
+    factor = re.compile("[\+\-\s][0-9]+[.]{0,1}[0-9]*")
+     
     for line in problem:
-        if(re.match(pattern, line) and cons==False):
+        if(re.match("min:", line) and cons==False):
             NoV += line.count('+')
             NoV += line.count('-')
             cons = True
@@ -79,27 +86,35 @@ def getProblemParams(file_path):
                 ff = float(f)
                 if(ff > max):
                     max = ff
-                if(abs(ff) < abs(min)):
+                if(abs(ff) < abs(min) and abs(ff) != 0):
                     min = ff
         elif(re.match(pattern, line)):
             NoC += 1
-            NoViC += line.count('+')
-            NoViC += line.count('-')
-            NoViC += 1
+            if(line.count('<') == 1):
+                ls = line.rsplit('<', -1)[0]    #ls = left side
+                NoViC += ls.count('+')
+                NoViC += ls.count('-')
+            elif(line.count('>') == 1):
+                ls = line.rsplit('>', -1)[0]    #ls = left side
+                NoViC += ls.count('+')
+                NoViC += ls.count('-')            
             factors = re.findall(factor, line)
             for f in factors:
                 ff = float(f)
                 if(ff > max):
                     max = ff
-                if(abs(ff) < abs(min)):
+                if(abs(ff) < abs(min) and abs(ff) != 0):
                     min = ff
-    
+     
     results.write("Number of variables: " + NoV.__str__() + "\n")
     results.write("Number of constraints: " + NoC.__str__() + "\n")
     CD = (NoViC.__float__() / (NoV * NoC)).__str__()
     results.write("Constraints density: " + CD + "\n")
     results.write("Maximum factor: " + max.__str__() + "\n")
     results.write("Minimum factor: " + min.__str__() + "\n")
+    
+    problem.close()
+    results.close()
     
 def scanOutput(solver):
     if(solver["id"] == "lp_solve" or solver["id"] == "glpk" or solver["id"] == "cplex"):
@@ -112,27 +127,55 @@ def scanOutput(solver):
         for x in d["VotV"]:
             file.write(x.__str__() + " x" + i.__str__() + " [" + solver["id"] + "]\n")
             i += 1
-           
-def main(argv):    
-    solvers = getConfiguredSolvers()
-    r = open("RESULTS", "w")
-    r.write("")
-    r.close()
-    getProblemParams(argv[-1])
+            
+def cleanResults():
+    os.chdir("results")
+    for file in glob.glob("*"):
+        os.unlink(file)
     
-    if(len(argv) < 3):
-        print("Program needs at least 2 arguments")
+    os.chdir("../")
+           
+def main(argv):
+    solvers = getConfiguredSolvers()
+    cleanResults()
+    global source
+    
+    if(argv[-1].startswith("--")):
+        source = "dir"
+    else:
+        source = "file"
+   
+    if(source == "file"):
+        getProblemParams(argv[-1])
+    else:
+        os.chdir("problems")
+        for file in glob.glob("*"):
+            getProblemParams(file)
+    
+        os.chdir("../")
+     
+    if(len(argv) < 2):
+        print("Program needs at least 1 arguments")
         return
-    elif(argv[1] == "--all"):
+    elif(argv[1] == "--all" and source == "file"):
         for solver in solvers:
             runSolver(solver, argv[-1])
         for solver in solvers:
             scanOutput(solver)
-    else:
+    elif(source == "file"):
         for solver in argv[1:-1]:
             item = (item for item in solvers if item["id"] == solver[2:]).next()
             runSolver(item, argv[-1])
             scanOutput(solver)
+    elif(argv[1] == "--all" and source == "dir"):
+        return
+    elif(source == "dir"):
+        listdir = os.listdir(script_dir + "/problems")
+        for solver in argv[1:]:
+            item = (item for item in solvers if item["id"] == solver[2:]).next()
+            for file in listdir:
+                runSolver(item, file)
+            #scanOutput(solver) 
                        
 if __name__ == "__main__":
     main(sys.argv)
--- input_translator.py
+++ input_translator.py
@@ -1,41 +1,58 @@
 import re
 
-def lp_solve(file):
-    base_file = open(file, 'r')
-    new_file = open("lp_solve_problem.lp", 'w')
-    pattern = re.compile(".+[:]")
-    general = False
-    
-    for line in base_file:
-        if(general and line.lower().strip() != "end"):
-            new_file.write(line[:-1] + ";\n")
-            general = False
-        elif(line.lower() == 'maximize\n' or line.lower() == 'maximum\n' or line.lower() == 'max\n'):
-            new_file.write("max:")
-        elif(re.match(pattern, line) ):
-            new_file.write(line.rsplit(':', -1)[1][:-1] + ";\n")
-        elif(line.lower().strip() == "subject to" or line.lower().strip() == "bounds" or line.lower().strip() == "end"):
-            new_file.write("")
-        elif(line.lower().strip() == "general"):
-            new_file.write("int ")
-            general = True
-        elif(len(line.strip()) > 1): 
-            if(line.strip()[-2] != ';' ):
-                new_file.write(line[:-1] + ";\n")
-        else:
-            new_file.write(line)
-            
-    base_file.close()
-    new_file.close()
-    return "lp_solve_problem.lp"
+# def lp_solve(file):
+#     base_file = open(file, 'r')
+#     new_file = open("lp_solve_problem.lp", 'w')
+#     pattern = re.compile(".+[:]")
+#     general = False
+#     
+#     for line in base_file:
+#         if(general and line.lower().strip() != "end"):
+#             new_file.write(line[:-1] + ";\n")
+#             general = False
+#         elif(line.lower() == 'maximize\n' or line.lower() == 'maximum\n' or line.lower() == 'max\n'):
+#             new_file.write("max:")
+#         elif(re.match(pattern, line) ):
+#             new_file.write(line.rsplit(':', -1)[1][:-1] + ";\n")
+#         elif(line.lower().strip() == "subject to" or line.lower().strip() == "bounds" or line.lower().strip() == "end"):
+#             new_file.write("")
+#         elif(line.lower().strip() == "general"):
+#             new_file.write("int ")
+#             general = True
+#         elif(len(line.strip()) > 1): 
+#             if(line.strip()[-2] != ';' ):
+#                 new_file.write(line[:-1] + ";\n")
+#         else:
+#             new_file.write(line)
+#             
+#     base_file.close()
+#     new_file.close()
+#     return "lp_solve_problem.lp"
+
+# def cplex(file):
+#     new_file = open(file[:-2] + "cmd", 'w')
+#     new_file.write("read " + file + "\n")
+#     new_file.write("opt\n")
+#     new_file.write("write cplex.sol\n")
+#     new_file.write("y\n")
+#     new_file.write("quit\n")
+#     new_file.write("\n")
+#     return file[:-2] + "cmd"
+#     
 
 def cplex(file):
-    new_file = open(file[:-2] + "cmd", 'w')
-    new_file.write("read " + file + "\n")
-    new_file.write("opt\n")
-    new_file.write("write cplex.sol\n")
-    new_file.write("y\n")
-    new_file.write("quit\n")
-    new_file.write("\n")
-    return file[:-2] + "cmd"
+    base_file = open(file, 'r')
+    new_file = open("cplex" + file, 'w')
+    constrait = re.compile("\w+:")
+    
+    for line in base_file:
+        if(re.match("min:", line)):
+            new_file.write("Minimize\n")
+            new_file.write("obj: " + line.rsplit(":",-1)[1][:-2] + "\n")
+            new_file.write("Subject To\n")
+        elif(constrait.match(line) != None):
+            new_file.write(line[:-2] + "\n" )
+    
+    new_file.write("\nEnd")
+        
     
