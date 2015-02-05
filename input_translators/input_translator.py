7c371796560c8fe3c621494abda6850a51d7ea8c
skony <piotr7skonieczny@gmail.com>
2015-02-05 15:59:32 +0100

    commit

--- input_translators/input_translator.py
+++
@@ -1,7 +0,0 @@
-def lp_solve(file):
-    base_file = open(file, 'r')
-    new_file = open("lp_solve_problem.lp", 'w')   
-    for line in base_file:
-        print line
-        new_file.write(line)
-        #if any(line in s for s in ['MAXIMIZE'])
